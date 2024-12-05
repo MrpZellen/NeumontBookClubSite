c43b176dd4f2030b7a42e636e182e3395d6a8158
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2024-12-04 17:10:31 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,498 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//go:generate npm run build
+	fmt.Println("Works!")
+	ginStartup()
+}
+
+func ginStartup() {
+	router := gin.Default()
+	router.Static("/css", "./static/css")
+	router.HTMLRender = createMyrender()
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "home", gin.H{
+			"title": "Book Club",
+		})
+	})
+	router.GET("/about", func(c *gin.Context) {
+		c.HTML(200, "about", gin.H{
+			"title": "About Me!",
+			"name":  "Benjamin Leonard",
+		})
+	})
+	router.GET("/bookList", func(c *gin.Context) {
+		c.HTML(200, "bookList", gin.H{
+			"title":         "Past Reads!",
+			"bookListItems": callBookList(),
+		})
+	})
+	router.GET("/forgot", func(c *gin.Context) {
+		c.HTML(200, "errorPage", gin.H{})
+	})
+	router.Run(":3344")
+}
+func createMyrender() multitemplate.Renderer {
+	render := multitemplate.NewRenderer()
+	render.AddFromFiles("about", "static/base.html", "pages/about.html")
+	render.AddFromFiles("home", "static/base.html", "pages/home.html")
+	render.AddFromFiles("bookList", "static/base.html", "pages/bookList.html")
+	render.AddFromFiles("errorPage", "pages/errorPage.html")
+	return render
+}
+
+// new api call works unlike old tester, missing parsing data in for statement
+func callBookList() [3]interface{} {
+	//UPDATE FOR BOOK LIST SIZE
+	bookSearchTerms := []map[string]interface{}{
+		{
+			"author": "Slayton",
+			"volume": "white_trash_warlock",
+		},
+		{
+			"author": "Bennett",
+			"volume": "tainted_cup",
+		},
+		{
+			"author": "Silvera",
+			"volume": "they_both_die_at_the_end",
+		},
+	}
+	var itemList [3]interface{}
+	for index, element := range bookSearchTerms {
+		var reqLink string = "https://www.googleapis.com/books/v1/volumes?q=" + element["volume"].(string) + "+inauthor:" + element["author"].(string) + "&key=AIzaSyBXxTN_Z3M7IIUdTi2JbfKRQAb413nlDTg"
+		req, err := http.NewRequest(http.MethodGet, reqLink, nil)
+		if err != nil {
+			log.Fatalf("request failed")
+		}
+		req.Header.Add("Content-type", "application/json; charset=utf-8")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("Failed to send HTTP request")
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Failed to read response body")
+		}
+		defer resp.Body.Close()
+		data := make(map[string]interface{})
+		json.Unmarshal(body, &data)
+		//data printing
+		pr := &itemList[index]
+		*pr = data["items"].([]interface{})[0]
+		// for key, value := range data {
+		// 	fmt.Printf("%s: %v\n", key, value)
+		// }
+	}
+	return itemList
+}
+
+type APIstructure struct {
+	Kind       string `json:"kind"`
+	TotalItems int    `json:"totalItems"`
+	Items      []struct {
+		Kind       string `json:"kind"`
+		ID         string `json:"id"`
+		Etag       string `json:"etag"`
+		SelfLink   string `json:"selfLink"`
+		VolumeInfo struct {
+			Title               string   `json:"title"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			AverageRating       float64  `json:"averageRating"`
+			RatingsCount        int      `json:"ratingsCount"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+			ListPrice   struct {
+				Amount       float64 `json:"amount"`
+				CurrencyCode string  `json:"currencyCode"`
+			} `json:"listPrice"`
+			RetailPrice struct {
+				Amount       float64 `json:"amount"`
+				CurrencyCode string  `json:"currencyCode"`
+			} `json:"retailPrice"`
+			BuyLink string `json:"buyLink"`
+			Offers  []struct {
+				FinskyOfferType int `json:"finskyOfferType"`
+				ListPrice       struct {
+					AmountInMicros int    `json:"amountInMicros"`
+					CurrencyCode   string `json:"currencyCode"`
+				} `json:"listPrice"`
+				RetailPrice struct {
+					AmountInMicros int    `json:"amountInMicros"`
+					CurrencyCode   string `json:"currencyCode"`
+				} `json:"retailPrice"`
+				Giftable bool `json:"giftable"`
+			} `json:"offers"`
+		} `json:"saleInfo,omitempty"`
+		AccessInfo struct {
+			Country                string `json:"country"`
+			Viewability            string `json:"viewability"`
+			Embeddable             bool   `json:"embeddable"`
+			PublicDomain           bool   `json:"publicDomain"`
+			TextToSpeechPermission string `json:"textToSpeechPermission"`
+			Epub                   struct {
+				IsAvailable  bool   `json:"isAvailable"`
+				AcsTokenLink string `json:"acsTokenLink"`
+			} `json:"epub"`
+			Pdf struct {
+				IsAvailable bool `json:"isAvailable"`
+			} `json:"pdf"`
+			WebReaderLink       string `json:"webReaderLink"`
+			AccessViewStatus    string `json:"accessViewStatus"`
+			QuoteSharingAllowed bool   `json:"quoteSharingAllowed"`
+		} `json:"accessInfo"`
+		SearchInfo struct {
+			TextSnippet string `json:"textSnippet"`
+		} `json:"searchInfo"`
+		VolumeInfo0 struct {
+			Title               string   `json:"title"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo0 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo1 struct {
+			Title               string   `json:"title"`
+			Subtitle            string   `json:"subtitle"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo1 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo2 struct {
+			Title               string   `json:"title"`
+			Subtitle            string   `json:"subtitle"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo2 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo3 struct {
+			Title               string   `json:"title"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo3 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		SaleInfo4 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo4 struct {
+			Title               string   `json:"title"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo5 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo5 struct {
+			Title               string   `json:"title"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo6 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo6 struct {
+			Title               string   `json:"title"`
+			Authors             []string `json:"authors"`
+			Publisher           string   `json:"publisher"`
+			PublishedDate       string   `json:"publishedDate"`
+			Description         string   `json:"description"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PageCount           int      `json:"pageCount"`
+			PrintType           string   `json:"printType"`
+			Categories          []string `json:"categories"`
+			MaturityRating      string   `json:"maturityRating"`
+			AllowAnonLogging    bool     `json:"allowAnonLogging"`
+			ContentVersion      string   `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo7 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+		VolumeInfo7 struct {
+			Title               string `json:"title"`
+			PublishedDate       string `json:"publishedDate"`
+			IndustryIdentifiers []struct {
+				Type       string `json:"type"`
+				Identifier string `json:"identifier"`
+			} `json:"industryIdentifiers"`
+			ReadingModes struct {
+				Text  bool `json:"text"`
+				Image bool `json:"image"`
+			} `json:"readingModes"`
+			PrintType           string `json:"printType"`
+			MaturityRating      string `json:"maturityRating"`
+			AllowAnonLogging    bool   `json:"allowAnonLogging"`
+			ContentVersion      string `json:"contentVersion"`
+			PanelizationSummary struct {
+				ContainsEpubBubbles  bool `json:"containsEpubBubbles"`
+				ContainsImageBubbles bool `json:"containsImageBubbles"`
+			} `json:"panelizationSummary"`
+			ImageLinks struct {
+				SmallThumbnail string `json:"smallThumbnail"`
+				Thumbnail      string `json:"thumbnail"`
+			} `json:"imageLinks"`
+			Language            string `json:"language"`
+			PreviewLink         string `json:"previewLink"`
+			InfoLink            string `json:"infoLink"`
+			CanonicalVolumeLink string `json:"canonicalVolumeLink"`
+		} `json:"volumeInfo,omitempty"`
+		SaleInfo8 struct {
+			Country     string `json:"country"`
+			Saleability string `json:"saleability"`
+			IsEbook     bool   `json:"isEbook"`
+		} `json:"saleInfo,omitempty"`
+	} `json:"items"`
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['pages/*.html', 'static/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
